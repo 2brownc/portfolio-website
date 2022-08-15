f9740b28616c0ee27eac78c3fc71acc0af2bee75
brown <86018533+2brownc@users.noreply.github.com>
2022-08-16 03:00:30 +0530

    add a footer

---
+++ components/Footer/Footer.tsx
@@ -0,0 +1,82 @@
+import { createStyles, Anchor, Group, ActionIcon } from '@mantine/core';
+import { IconBrandTwitter, IconBrandYoutube, IconBrandInstagram } from '@tabler/icons';
+import { MantineLogo } from '@mantine/ds';
+import Link from 'next/link';
+
+import Logo from '../Logo/Logo';
+
+const useStyles = createStyles((theme) => ({
+  footer: {
+    borderTop: `1px solid ${theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.gray[2]
+      }`,
+  },
+
+  inner: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: `${theme.spacing.md}px ${theme.spacing.md}px`,
+
+    [theme.fn.smallerThan('sm')]: {
+      flexDirection: 'column',
+    },
+  },
+
+  links: {
+    [theme.fn.smallerThan('sm')]: {
+      marginTop: theme.spacing.lg,
+      marginBottom: theme.spacing.sm,
+    },
+  },
+}));
+
+interface FooterCenteredProps {
+  links: { link: string; label: string }[];
+}
+
+export default function FooterCentered({ links }: FooterCenteredProps) {
+  const { classes } = useStyles();
+  const items = links.map((link) => (
+    <Link
+      href={link.link}
+      key={link.label}
+    >
+      <Anchor<'a'>
+        color="dimmed"
+        sx={{ lineHeight: 1 }}
+        size="sm"
+      >
+        {link.label}
+      </Anchor>
+    </Link>
+  ));
+
+  return (
+    <div className={classes.footer}>
+      <div className={classes.inner}>
+        {/* <Logo /> */}
+
+        <Group
+          className={classes.links}
+          position="center"
+        >
+          {items}
+        </Group>
+
+        {/*
+        <Group spacing="xs" position="right" noWrap>
+          <ActionIcon size="lg" variant="default" radius="xl">
+            <IconBrandTwitter size={18} stroke={1.5} />
+          </ActionIcon>
+          <ActionIcon size="lg" variant="default" radius="xl">
+            <IconBrandYoutube size={18} stroke={1.5} />
+          </ActionIcon>
+          <ActionIcon size="lg" variant="default" radius="xl">
+            <IconBrandInstagram size={18} stroke={1.5} />
+          </ActionIcon>
+        </Group>
+          */}
+      </div>
+    </div>
+  );
+}
