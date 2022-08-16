330f19cf65c61db948d7d0fbd801ef24e653a43f
brown <86018533+2brownc@users.noreply.github.com>
2022-08-16 21:58:07 +0530

    hide search bar till control panel is implemented

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -107,12 +107,14 @@ export default function HeaderSearch({ links }: HeaderSearchProps) {
           <Group ml={50} spacing={5} className={classes.links}>
             {items}
           </Group>
+          {/*
           <Autocomplete
             className={classes.search}
             placeholder="Search"
             icon={<IconSearch size={16} stroke={1.5} />}
             data={['React', 'Angular', 'Vue', 'Next.js', 'Riot.js', 'Svelte', 'Blitz.js']}
           />
+            */}
           <Group className={classes.colorSchemeToggle}>
             <ColorSchemeSegmentedToggle />
           </Group>
