55434007f7b5c8bbdee1ebd3a1c6cd955bd85590
brown <86018533+2brownc@users.noreply.github.com>
2022-08-17 11:40:42 +0530

    add border to home page section

--- pages/index.jsx
+++ pages/index.jsx
@@ -6,6 +6,7 @@ import {
   Container,
   Grid,
   Button,
+  Paper,
 } from '@mantine/core';
 
 import Link from 'next/link';
@@ -16,57 +17,63 @@ export default function IndexPage() {
   return (
     <Center className={Styles.main}>
       <Container>
-        <Grid>
-          <Grid.Col md={6}>
-            <Stack>
-              <Stack
-                spacing="xs"
-                align="center"
-              >
-                <Title
-                  order={1}
-                  mb={0}
-                >
-                  Brown Chinta
-                </Title>
-                <Text
-                  size="lg"
-                  align="center"
-                >
-                  Hi! I am a software developer.
-                </Text>
-
-                <Text
-                  size="md"
-                  align="center"
-                >
-                  Look around my website and get to know me more.
-                </Text>
-              </Stack>
-            </Stack>
-          </Grid.Col>
-          <Grid.Col md={6}>
-            <Stack align="center">
+        <Paper
+          shadow="md"
+          p="xl"
+          withBorder
+        >
+          <Grid>
+            <Grid.Col md={6}>
               <Stack>
-                <Link href="/projects" passHref>
-                  <Button>
-                    Look at my Projects
-                  </Button>
-                </Link>
-                <Link href="/resume" passHref>
-                  <Button>
-                    View Resume / Contact Info
-                  </Button>
-                </Link>
-                <Link href="/blog" passHref>
-                  <Button>
-                    Read my Blog
-                  </Button>
-                </Link>
+                <Stack
+                  spacing="xs"
+                  align="center"
+                >
+                  <Title
+                    order={1}
+                    mb={0}
+                  >
+                    Brown Chinta
+                  </Title>
+                  <Text
+                    size="lg"
+                    align="center"
+                  >
+                    Hi! I am a software developer.
+                  </Text>
+
+                  <Text
+                    size="md"
+                    align="center"
+                  >
+                    Look around my website and get to know me more.
+                  </Text>
+                </Stack>
               </Stack>
-            </Stack>
-          </Grid.Col>
-        </Grid>
+            </Grid.Col>
+            <Grid.Col md={6}>
+              <Stack align="center">
+                <Stack>
+                  <Link href="/projects" passHref>
+                    <Button>
+                      Look At My Projects
+                    </Button>
+                  </Link>
+                  <Link href="/resume" passHref>
+                    <Button>
+                      View Resume / Contact Info
+                    </Button>
+                  </Link>
+                  <Link href="/blog" passHref>
+                    <Button>
+                      Read My Blog
+                    </Button>
+                  </Link>
+                </Stack>
+              </Stack>
+            </Grid.Col>
+          </Grid>
+        </Paper>
       </Container>
     </Center>
   );
