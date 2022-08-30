1b9671343f226d8780e8e7496ddb8bc815dcb30b
brown <86018533+2brownc@users.noreply.github.com>
2022-08-30 22:55:28 +0530

    enable code titles

--- components/Blog/Blog.jsx
+++ components/Blog/Blog.jsx
@@ -82,6 +82,9 @@ export default function BlogLayout({
       img: {
         border: `1px solid ${theme.colorScheme === 'light' ? theme.colors.gray[3] : theme.colors.gray[6]}`,
         background: theme.colorScheme === 'light' ? theme.colors.gray[2] : theme.colors.gray[7],
+      },
+      '.rehype-code-title': {
+        color: theme.colorScheme === 'light' ? theme.colors.gray[5] : theme.colors.gray[7],
       }
     }
   }));
